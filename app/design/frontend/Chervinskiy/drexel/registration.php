7c466d950233754cf5d7ecb7f6512563a1eb918d
Kuro <st.cherkiy@gmail.com>
2022-12-13 11:13:15 +0200

    Initial commit
    Initial commit included with first task

---
+++ app/design/frontend/Chervinskiy/drexel/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Chervinskiy/drexel', __DIR__);
---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  drexel: {
+    area: 'frontend',
+    name: 'Chervinskiy/drexel',
+    locale: 'en_US',
+    files: [ 'css/styles-m', 'css/styles-l' ],
+    dsl: 'less'
+  }
+}
